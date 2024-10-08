c8def5291f8357e418ab647c868729c23e0f8f8e
Aurelien Andre <aurelien.andre@aartintelligent.fr>
2024-10-08 19:45:38 +0200

    initial commit

---
+++ addons/aartintelligent_exercice/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'AArtIntelligent Excercice',
+    'summary': 'Todot',
+    'version': '1.0.0',
+    'category': 'Hidden',
+    'author': 'AArtIntelligent',
+    'website': 'https://aartintelligent.com',
+    'support': 'aurelien.andre@aartintelligent.com',
+    'license': 'OPL-1',
+    'price': 0.00,
+    'currency': 'EUR',
+    'depends': [
+        'base',
+    ],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
